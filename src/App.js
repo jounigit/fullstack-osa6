b72a8078cd3fd495751055fd07878dd32c6b1c8b
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-07-24 12:10:14 +0300

    6.10 anekdootit ja backend, osa 1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import { connect } from 'react-redux'
+import { anecdoteInit } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 class App extends React.Component {
+  componentDidMount = async () => {
+    const anecdotes = await anecdoteService.getAll()
+    this.props.anecdoteInit(anecdotes)
+  }
 
   render() {
     //const anecdotes = this.props.store.getState()
@@ -18,4 +25,7 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default connect(
+  null,
+  { anecdoteInit }
+)(App)
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,25 +1,8 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000*Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+//const initialState = anecdotesAtStart.map(asObject)
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (store = initialState, action) => {
+const anecdoteReducer = (store = [], action) => {
   if (action.type==='VOTE') {
     const old = store.filter(a => a.id !==action.id)
     const voted = store.find(a => a.id === action.id)
@@ -29,8 +12,18 @@ const anecdoteReducer = (store = initialState, action) => {
   if (action.type === 'CREATE') {
     return [...store, { content: action.content, id: getId(), votes:0 }]
   }
+  if (action.type === 'INIT') {
+    return action.content
+  }
 
   return store
+}
+
+export const anecdoteInit = (content) => {
+  return {
+    type: 'INIT',
+    content
+  }
 }
 
 export const createAnecdote = (content) => {
