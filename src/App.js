311cb7d69ec39a3818d81c0ab8b83edfb184d7c1
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-07-26 16:49:22 +0300

    6.22 styled anecdotes, osa 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
-import { Container, Table } from 'semantic-ui-react'
+import { Container, Table, Grid, Image } from 'semantic-ui-react'
 
 const Menu = ({ style }) => (
   <div style={ style }>
@@ -42,14 +42,25 @@ const AnecdoteList = ({ anecdotes }) => (
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={1}>
+        </Grid.Column>
+        <Grid.Column width={9}>
+          <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is { ' "a story with a point." ' }</em>
+          <em>An anecdote is a brief, revealing account of an individual person or an incident.
+            Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+            such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+            An anecdote is { ' "a story with a point." ' }</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+          <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <Image src='/images/Alan_Turing.jpg' />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   </div>
 )
 
@@ -173,9 +184,7 @@ class App extends React.Component {
       fontWeight: 'bold',
       textDecoration: 'none',
       color: 'white',
-      border: 'solid',
       backgroundColor: 'DarkKhaki',
-      borderColor: 'olive',
       margin: 5,
       padding: 10
     }
